af5756c4c1182a2518003df8213071155c6d65c6
suvel <vdr_Suvelrathneswar@airasia.com>
2023-09-02 10:29:32 +0530

    Added AST explorer notes

---
+++ AST_explorer_notes.js
@@ -0,0 +1,203 @@
+{
+  "type": "JSXOpeningElement",
+  "start": 0,
+  "end": 41,
+  "loc": {
+    "start": {
+      "line": 1,
+      "column": 0,
+      "index": 0
+    },
+    "end": {
+      "line": 1,
+      "column": 41,
+      "index": 41
+    }
+  },
+  "name": {
+    "type": "JSXIdentifier",
+    "start": 1,
+    "end": 6,
+    "loc": {
+      "start": {
+        "line": 1,
+        "column": 1,
+        "index": 1
+      },
+      "end": {
+        "line": 1,
+        "column": 6,
+        "index": 6
+      }
+    },
+    "name": "Route"
+  },
+  "attributes": [
+    {
+      "type": "JSXAttribute",
+      "start": 7,
+      "end": 20,
+      "loc": {
+        "start": {
+          "line": 1,
+          "column": 7,
+          "index": 7
+        },
+        "end": {
+          "line": 1,
+          "column": 20,
+          "index": 20
+        }
+      },
+      "name": {
+        "type": "JSXIdentifier",
+        "start": 7,
+        "end": 11,
+        "loc": {
+          "start": {
+            "line": 1,
+            "column": 7,
+            "index": 7
+          },
+          "end": {
+            "line": 1,
+            "column": 11,
+            "index": 11
+          }
+        },
+        "name": "path"
+      },
+      "value": {
+        "type": "StringLiteral",
+        "start": 12,
+        "end": 20,
+        "loc": {
+          "start": {
+            "line": 1,
+            "column": 12,
+            "index": 12
+          },
+          "end": {
+            "line": 1,
+            "column": 20,
+            "index": 20
+          }
+        },
+        "extra": {
+          "rawValue": "Route2",
+          "raw": "\"Route2\""
+        },
+        "value": "Route2"
+      }
+    },
+    {
+      "type": "JSXAttribute",
+      "start": 21,
+      "end": 39,
+      "loc": {
+        "start": {
+          "line": 1,
+          "column": 21,
+          "index": 21
+        },
+        "end": {
+          "line": 1,
+          "column": 39,
+          "index": 39
+        }
+      },
+      "name": {
+        "type": "JSXIdentifier",
+        "start": 21,
+        "end": 28,
+        "loc": {
+          "start": {
+            "line": 1,
+            "column": 21,
+            "index": 21
+          },
+          "end": {
+            "line": 1,
+            "column": 28,
+            "index": 28
+          }
+        },
+        "name": "element"
+      },
+      "value": {
+        "type": "JSXExpressionContainer",
+        "start": 29,
+        "end": 39,
+        "loc": {
+          "start": {
+            "line": 1,
+            "column": 29,
+            "index": 29
+          },
+          "end": {
+            "line": 1,
+            "column": 39,
+            "index": 39
+          }
+        },
+        "expression": {
+          "type": "JSXElement",
+          "start": 30,
+          "end": 38,
+          "loc": {
+            "start": {
+              "line": 1,
+              "column": 30,
+              "index": 30
+            },
+            "end": {
+              "line": 1,
+              "column": 38,
+              "index": 38
+            }
+          },
+          "openingElement": {
+            "type": "JSXOpeningElement",
+            "start": 30,
+            "end": 38,
+            "loc": {
+              "start": {
+                "line": 1,
+                "column": 30,
+                "index": 30
+              },
+              "end": {
+                "line": 1,
+                "column": 38,
+                "index": 38
+              }
+            },
+            "name": {
+              "type": "JSXIdentifier",
+              "start": 31,
+              "end": 36,
+              "loc": {
+                "start": {
+                  "line": 1,
+                  "column": 31,
+                  "index": 31
+                },
+                "end": {
+                  "line": 1,
+                  "column": 36,
+                  "index": 36
+                }
+              },
+              "name": "Comp2"
+            },
+            "attributes": [],
+            "selfClosing": true
+          },
+          "closingElement": null,
+          "children": []
+        }
+      }
+    }
+  ],
+  "selfClosing": true
+}
